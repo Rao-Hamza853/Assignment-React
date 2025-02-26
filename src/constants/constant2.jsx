73d23d3460ce0f280f707b0a703b9d00653d20c8
unknown <raohamza936@gmail.com>
2025-02-26 16:06:37 +0500

    tasks complete

---
+++ src/constants/constant2.jsx
@@ -0,0 +1,50 @@
+export const tasks = [
+    {
+      title : "Reuse Component in Loop",
+      path : "/reuse-comp"
+    },
+    {
+      title : "Array Nested Looping",
+      path : "/nested-array-loop"
+    },
+    {
+      title : "Manage Form Inputs",
+      path : "/manage-form-inputs"
+    },
+    {
+      title : "Fetch API",
+      path : "/fetch-api"
+    },
+    {
+      title : "State Side Effect",
+      path : "/state-side-effect"
+    },
+    {
+      title : "Prop Side Effect",
+      path : "/prop-side-effect"
+    },
+    {
+      title : "Life Cycle Method",
+      path : "/life-cycle-method"
+    },
+    {
+      title : "Focus Input Field",
+      path : "/useref-hook"
+    },
+    {
+      title : "Uncontrolled Component",
+      path : "/useref-hook"
+    },
+    {
+      title : "Pass Function As Props",
+      path : "/last-three-tasks"
+    },
+    {
+      title : "Forward Ref in React",
+      path : "/last-three-tasks"
+    },
+    {
+      title : "Use Form Status Hook",
+      path : "/last-three-tasks"
+    }
+  ]
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,72 +1,22 @@
 import { Link } from "react-router"
 import { MdArrowOutward } from "react-icons/md";
+import { tasks } from "../constants/constant2";
 
+const Home = () => { 
 
-const Home = () => {
   return (
     <div className="bg-gray-200 h-screen">
         <h2 className="text-4xl text-center font-bold pt-5 mb-8 text-[#0A5EB0]">Assignment React JS</h2>
-        <div className="grid grid-cols-3 mx-[150px] gap-4">
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Reuse Component in Loop</h3>
-            <Link to="/reuse-comp"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Array Nested Looping</h3>
-            <Link to="/nested-array-loop"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Manage Form Inputs</h3>
-            <Link to="/manage-form-inputs"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Fetch API</h3>
-            <Link to="/fetch-api"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">State Side Effect</h3>
-            <Link to="/state-side-effect"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Prop Side Effect</h3>
-            <Link to="/prop-side-effect"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Life Cycle Method</h3>
-            <Link to="/life-cycle-method"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Focus Input Field</h3>
-            <Link to="/useref-hook"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Uncontrolled Component</h3>
-            <Link to="/useref-hook"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Pass Function As Props</h3>
-            <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Forward Ref in React</h3>
-            <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
-          <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
-            <h3 className="text-2xl font-semibold text-blue-00">Use Form Status Hook</h3>
-            <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
-          </div>
+        <div className="grid grid-cols-3 mx-40 gap-4">
+          {
+            tasks.map((task,index)=>(
+              <div className='p-4 bg-white shadow-md rounded-lg border border-gray-200 space-y-3'>
+                <h3 className="text-2xl font-semibold text-blue-00">{task.title}</h3>
+                <Link to={task.path}><button className="border bg-blue-500 text-white text-lg font-medium px-1.5 py-0.5 cursor-pointer rounded-lg hover:bg-white hover:text-blue-500 flex items-center gap-x-1"  >Go to task <MdArrowOutward /></button></Link>
+              </div>
+            ))
+          }
         </div> 
-      {/* <Link to="/nested-array-loop"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Array Nested Looping</button></Link>
-      <Link to="/manage-form-inputs"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Manage Form Inputs</button></Link>
-      <Link to="/fetch-api"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Fetch API</button></Link>
-      <Link to="/state-side-effect"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">State Side Effect</button></Link>
-      <Link to="/prop-side-effect"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Prop Side Effect</button></Link>
-      <Link to="/life-cycle-method"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Life Cycle Method</button></Link>
-      <Link to="/useref-hook"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Focus Input Field</button></Link>
-      <Link to="/useref-hook"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Uncontrolled Component</button></Link>
-      <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Pass Function As Props</button></Link>
-      <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Forward Ref</button></Link>
-      <Link to="/last-three-tasks"><button className="border bg-blue-500 text-white text-lg font-medium px-3 py-1 cursor-pointer">Use Form Status Hook</button></Link> */}
     </div>
   )
 }
